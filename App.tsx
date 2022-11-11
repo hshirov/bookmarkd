bf53accf929a9ce07a781f56e763cf430aeeb2bb
Hristo Shirov <hristo.shirov@mentormate.com>
2022-11-11 11:28:58 +0200

    Add config files

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+const App = () => (
+  <View>
+    <StatusBar style="auto" />
+    <Text>Open up App.js to start working on your app!</Text>
+  </View>
+);
+
+export default App;
