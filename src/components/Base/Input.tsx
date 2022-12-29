d2cb93de1964838f97e08d0ec0e07344f3c1089e
Hristo Shirov <hristoshirov@gmail.com>
2022-12-29 11:29:55 +0200

    Add autoCorrect prop to Input

--- src/components/Base/Input.tsx
+++ src/components/Base/Input.tsx
@@ -5,33 +5,37 @@ import Theme from 'enums/Theme.enum';
 
 interface InputProps {
   value: string;
+  autoCorrect?: boolean;
   style?: StyleProp<TextStyle>;
   onChangeText: (text: string) => void;
   // eslint-disable-next-line
   ref?: React.Ref<TextInput>;
 }
 
-const Input: React.FC<InputProps> = React.forwardRef<TextInput, InputProps>(({ value, style, onChangeText }, ref) => {
-  const { theme, colors, spacing, sizing } = useTheme();
+const Input: React.FC<InputProps> = React.forwardRef<TextInput, InputProps>(
+  ({ value, autoCorrect = false, style, onChangeText }, ref) => {
+    const { theme, colors, spacing, sizing } = useTheme();
 
-  return (
-    <TextInput
-      style={[
-        {
-          height: spacing.spacer * 3,
-          color: colors.text,
-          borderBottomColor: colors.text,
-          borderBottomWidth: sizing.borderWidth,
-          fontSize: sizing.fontSmall,
-        },
-        style,
-      ]}
-      keyboardAppearance={theme === Theme.Dark ? 'dark' : 'light'}
-      value={value}
-      ref={ref}
-      onChangeText={onChangeText}
-    />
-  );
-});
+    return (
+      <TextInput
+        style={[
+          {
+            height: spacing.spacer * 3,
+            color: colors.text,
+            borderBottomColor: colors.text,
+            borderBottomWidth: sizing.borderWidth,
+            fontSize: sizing.fontSmall,
+          },
+          style,
+        ]}
+        keyboardAppearance={theme === Theme.Dark ? 'dark' : 'light'}
+        autoCorrect={autoCorrect}
+        value={value}
+        ref={ref}
+        onChangeText={onChangeText}
+      />
+    );
+  }
+);
 
 export default Input;
