3f3ab157a4dc1206f34c0b5a1b9616ae0c735cc0
Hristo Shirov <hristoshirov@gmail.com>
2023-01-05 21:35:01 +0200

    Add no results message in explore screen

--- src/screens/Explore.tsx
+++ src/screens/Explore.tsx
@@ -8,6 +8,7 @@ import { GetBookResponse } from 'src/interfaces/api/responses.interface';
 import useBooksSearch from 'hooks/queries/useBooksSearch';
 import useDebounce from 'hooks/useDebounce';
 import useTheme from 'hooks/useTheme';
+import { isNonEmptyStr } from 'utils/index';
 import { Container, Input, Text } from 'components/Base';
 import { BookCard } from 'components/Book';
 import { ErrorScreen } from 'components/Common';
@@ -19,45 +20,45 @@ const Explore: React.FC<TabNavProps<TabRoute.Explore>> = ({ navigation }) => {
   const debouncedSearchQuery = useDebounce(searchQuery);
   const { data, isFetching, isError, fetchNextPage } = useBooksSearch(debouncedSearchQuery.trim());
 
-  const hasPages = data?.pages[0].items.length !== 0;
-
   useFocusEffect(
     useCallback(() => {
       searchInputRef.current?.focus();
     }, [searchInputRef])
   );
 
-  const content = useMemo(
-    () =>
-      hasPages ? (
-        <FlatList
-          data={data?.pages}
-          renderItem={({ item: page }) => (
-            <View>
-              {page.items?.map((item: GetBookResponse) => (
-                <BookCard
-                  key={item.id}
-                  title={item.volumeInfo.title}
-                  authors={item.volumeInfo.authors}
-                  imageUri={item.volumeInfo.imageLinks?.thumbnail}
-                  style={{ marginBottom: spacing.spacer }}
-                  onPress={() => navigation.navigate(TabRoute.BookDetails, { id: item.id })}
-                />
-              ))}
-            </View>
-          )}
-          ListFooterComponent={isFetching ? <ActivityIndicator /> : null}
-          ListFooterComponentStyle={{ marginTop: spacing.spacer }}
-          onEndReachedThreshold={0.5}
-          onEndReached={() => fetchNextPage()}
-        />
-      ) : (
-        <View style={{ alignItems: 'center' }}>
-          <Text.Secondary>Search by book title or author.</Text.Secondary>
-        </View>
-      ),
-    [hasPages, data, isFetching, spacing]
-  );
+  const content = useMemo(() => {
+    const hasPages = !!data?.pages[0]?.items?.length;
+
+    return hasPages || isFetching ? (
+      <FlatList
+        data={data?.pages}
+        renderItem={({ item: page }) => (
+          <View>
+            {page.items?.map((item: GetBookResponse) => (
+              <BookCard
+                key={item.id}
+                title={item.volumeInfo.title}
+                authors={item.volumeInfo.authors}
+                imageUri={item.volumeInfo.imageLinks?.thumbnail}
+                style={{ marginBottom: spacing.spacer }}
+                onPress={() => navigation.navigate(TabRoute.BookDetails, { id: item.id })}
+              />
+            ))}
+          </View>
+        )}
+        ListFooterComponent={isFetching ? <ActivityIndicator /> : null}
+        ListFooterComponentStyle={{ marginTop: spacing.spacer }}
+        onEndReachedThreshold={0.5}
+        onEndReached={() => fetchNextPage()}
+      />
+    ) : (
+      <View style={{ alignItems: 'center' }}>
+        <Text.Secondary>
+          {isNonEmptyStr(debouncedSearchQuery) ? 'No results found.' : 'Search by book title or author.'}
+        </Text.Secondary>
+      </View>
+    );
+  }, [data, isFetching, spacing]);
 
   return (
     <Container>
